817eb1792aae1c592b4e040fa7dcf9e867ca8ff5
Denis Feier <denisfeier@Deniss-MacBook-Pro.local>
2025-03-25 17:56:50 +0200

    start

---
+++ start.py
@@ -0,0 +1,70 @@
+from pygame import *
+from random import randint
+
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('fire.ogg')
+
+font.init()
+font1 = font.Font(None, 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+font2 = font.Font(None, 36)
+
+img_back = "galaxy.jpg"
+img_hero = "rocket.png"
+img_bullet = "bullet.png"
+img_enemy = "ufo.png"
+
+score = 0
+lost = 0
+max_lost = 3
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # We call the class constructor (Sprite):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    def fire(self):
+        pass
+
+
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+
+finish = False
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    if not finish:
+        window.blit(background, (0, 0))
+        ship.update()
+        ship.reset()
+        display.update()
+    time.delay(50)
